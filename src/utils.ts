d6911255277192a91146dd8d3e54df9cce856463
felix-lipski <felix.lipski7@gmail.com>
2021-06-09 06:19:22 +0000

    docs: add README.md, explanation in utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,9 @@
 import { StatObj, StatsArr } from "./types";
 
+// transform an array of object into and array of [key, val] pairs (entries)
+// where key is the value of the property grouped by, and val is an array of
+// all the objects in which the property grouped by equals to the key
+// (StatsArr, string) -> [[string, StatsArr]]
 export const groupByField = (statsArr: StatsArr, field: string) =>
   statsArr
     .reduce(
